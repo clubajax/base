fa96ea175be7cbc811e04ae327015713df0c693d
Mike Wilcox <mike@mikewilcox.net>
2013-12-07 15:25:28 -0600

    add has, on

---
+++ has.js
@@ -0,0 +1,46 @@
+define([], function(dcl){
+	
+	// has library
+	// 
+	var VENDOR_PREFIXES = ["Webkit", "Moz", "O", "ms", "Khtml"],
+		d = document,
+		el = d.createElement("DiV"),
+		testCache = {},
+		isIE = navigator.userAgent.indexOf('Trident') > -1
+    ;
+	
+	function has(name){
+        if(typeof testCache[name] == "function"){
+            testCache[name] = testCache[name](g, d, el);
+        }
+        return testCache[name];
+    }
+	
+	function add(name, test, now){
+        testCache[name] = now ? test(g, d, el) : test;
+    }
+	
+	
+	
+	
+	// has custom tests
+	// 
+	has.add('wheel', function(win, doc, div){
+        return  "onwheel" in div || "wheel" in div ||
+            (isIE && doc.implementation.hasFeature("Events.wheel", "3.0")); // IE feature detection
+    });
+
+    function mouseleave(){
+       var n = d.createElement('div');
+       n.setAttribute('onmouseleave');
+       return typeof n.onmouseleave === 'function';
+    }
+    has.add('mouseenter', function(){
+        return mouseleave();
+    });
+    has.add('mouseleave', function(){
+        return mouseleave();
+    });
+	
+	return has;
+});
