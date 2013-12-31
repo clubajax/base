a088355d9b469f6613e13498389d9f8b8a133c41
Mike Wilcox <mike@mikewilcox.net>
2013-12-31 17:16:39 -0600

    View headers and footers

--- ui/views/View.js
+++ ui/views/View.js
@@ -9,18 +9,40 @@ define([
 		
 		baseClass:'base-view',
 		className:'',
+		headerClass:'base-view-header',
+		containerClass:'base-view-container',
+		footerClass:'base-view-footer',
 		
-		template:'<div class="{{baseClass}} {{className}}"></div>',
 		
-		content:null, // widget or html
+		
+		template:'<div class="{{baseClass}} {{className}}">' +
+						'<div data-ref="headerNode" class="{{headerClass}}"></div>' +
+						'<div data-ref="containerNode" class="{{containerClass}}"></div>' +
+						'<div data-ref="footerNode" class="{{footerClass}}"></div>' +
+					'</div>',
+		
+		headerContent:null,
+		content:null,
+		footerContent:null,
 		
 		constructor: function(){
 			
 		},
 		
 		postRender: function(){
-			this.node.innerHTML = this.content || '';
-			parser.parse(this.node);
+			this.containerNode.innerHTML = this.content || '';
+			parser.parse(this.containerNode);
+			
+			if(this.headerContent){
+				this.headerNode.innerHTML = this.headerContent;
+				parser.parse(this.headerNode);
+				this.node.classList.add('hasHeader');
+			}
+			if(this.footerContent){
+				this.footerNode.innerHTML = this.footerContent;
+				parser.parse(this.footerNode);
+				this.node.classList.add('hasFooter');
+			}
 		}
 	});
 });
