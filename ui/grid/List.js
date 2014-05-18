4ff37b5b37f77a0eda2f03ee4b19a25bc8c09538
Mike Wilcox <mike@mikewilcox.net>
2014-05-18 10:58:26 -0500

    List test; Add optional header

--- ui/grid/List.js
+++ ui/grid/List.js
@@ -17,18 +17,19 @@ define([
 		
 		columns:null,
 		
-		//observables:{
-		//	title:'NOTSET'
-		//},
+		useHeader: false,
 		
 		baseClass:'base-list',
 		rowClass:'base-list-row',
 		headerClass:'base-list-header',
 		containerClass:'base-list-container',
+		scrollerClass:'base-list-scroller',
 		
-		template:'<div class="{{baseClass}}"><div data-ref="container" class="{{containerClass}}"></div></div>',
+		template:'<div class="{{baseClass}}"><div data-ref="container" class="{{containerClass}}"><div data-ref="scroller" class="{{scrollerClass}}"></div></div></div>',
 		
-		rowTemplate:'<div class="base-list-pair"><div class="base-list-label">{LABEL}</div><div class="base-list-text">{TEXT}</div></div>',
+		rowLabelValueTemplate:'<div class="base-list-pair"><div class="base-list-label">{LABEL}</div><div class="base-list-text">{TEXT}</div></div>',
+		rowLabelTemplate:'<div class="base-list-pair"><div class="base-list-label">{LABEL}</div></div>',
+		rowValueTemplate:'<div class="base-list-pair"><div class="base-list-text">{TEXT}</div></div>',
 		
 		constructor: function(){
 			this.setStore(this.store);
@@ -36,11 +37,6 @@ define([
 		
 		postRender: function(){
 			this.connectClickEvents();
-			
-			//this.title.sub(function(value){
-			//	this.header.innerHTML = value;
-			//}, this);
-			//this.title.pub();
 		},
 		
 		setStore: function(store){
@@ -56,23 +52,39 @@ define([
 			}
 		},
 		render: function(data, columns){
-			var i, col, row, html, label, text, key;
-			this.container.innerHTML = '';
+			var i, col, row, html, label, text, key,
+			template = this.useHeader ? this.rowValueTemplate : this.rowLabelValueTemplate,
+			node = this.container;
+			node.innerHTML = '';
 			if(columns){
 				this.columns = columns;
 			}
+			this.renderHeader(data);
 			for(i = 0; i < data.items.length; i++){
 				row = dom('div', {css:this.rowClass, attr:{'data-item-index':i}});
+				
 				for(col = 0; col < this.columns.length; col++){
 					label = this.columns[col].label;
 					key = this.columns[col].key || label;
 					text = data.items[i][key];
-					html = this.rowTemplate.replace('{LABEL}', label).replace('{TEXT}', text);
+					html = template.replace('{LABEL}', label).replace('{TEXT}', text);
 					row.innerHTML += html;
 				}
-				this.container.appendChild(row);
+				node.appendChild(row);
 			}
 		},
+		
+		renderHeader: function(){
+			if(!this.useHeader){ return; }
+			var col, label, template = this.rowLabelTemplate,
+			row = dom('div', {css:this.headerClass});
+			for(col = 0; col < this.columns.length; col++){
+				label = this.columns[col].label;
+				row.innerHTML += template.replace('{LABEL}', label);
+			}
+			this.node.appendChild(row);
+		},
+		
 		connectClickEvents: function(){
 			this.on(this.container, 'click .' + this.rowClass, function(event){
 				if(this.store){
@@ -86,6 +98,7 @@ define([
 				this.emit(events.rowClick, event);
 			}, this);
 		},
+		
 		query: function(){
 			this.store.query.apply(this, arguments);
 		}
