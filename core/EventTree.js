b0ad6aa4aa5c394145f577d6e08067b5e42e950a
Mike Wilcox <anm9tr@gmail.com>
2014-10-04 15:00:07 -0500

    Add auto-uid

--- core/EventTree.js
+++ core/EventTree.js
@@ -1,241 +1,265 @@
-	define([
-	'../dcl/dcl',
-	'./dom',
-	'./on'
-	], function(dcl, dom, on){
-	// EventTree
-	//      EventTree is a typical event emitter with additional functionality
-	//      of generating child EventTrees. A child event will propogate up the tree
-	//      to the original tree, but will not propogate down other branches.
-	//
-	//      A chain can be built by using setSource. Objects up the tree will
-	//      be added.
-	//
-	//      See EventTree-main.js in the tests for usage.
-	//      
+define([
+	'dcl/dcl'
+], function(dcl){
+    // EventTree
+    //      EventTree is a typical event emitter with additional functionality
+    //      of generating child trees. A child event will bubble up the tree,
+    //      but will not down other branches.
+    //
+    //      A chain can be built by using setSource. Objects up the tree will
+    //      be added.
+    //
+    //      EventTree methods are mixed into instances in localLib/Base. So most
+    //      in not all of EventTree operations should be transparent, except for
+    //      passing children:
+    //
+    //      new Graph({
+    //          eventTree: this.eventTree.child()
+    //      })
+    //
+    //      See EventTree-main.js in the tests for usage.
+    //
 	var
-		EventTree,
-		_id = 0;
-	function uid(str){
-		str = str || 'event-';
-		return str + (_id++);
-	}
-	function noop(){}
-	noop.name = 'noop';
-	
+        EventTree,
+        _id = 0,
+        registry = {};
+
+    function register(id, handle){
+        if(!registry[id]){
+            registry[id] = [];
+        }
+        registry[id].push(handle);
+    }
+
+    function uid(str){
+        str = str || 'event-';
+        return str + (_id++);
+    }
+
+    function noop(){}
+    noop.name = 'noop';
+
 	EventTree = dcl(null, {
-	
+
 		declaredClass:'EventTree',
-		eventNames:null,
-	
+        eventNames:null,
+
 		constructor: function(options){
-			options = options || {};
-			this.id = uid('event-tree-');
+            options = options || {};
+            this.id = uid((this.id || 'event-tree') + '-');
 			this.listeners = {};
-			this.handles = {};
-			this.children = {};
-			this._previousEvents = {};
-			if(options.events){
-				this.setEventNames(options.events);
-			}
-			if(options.source){
-				this.setSource(options.sourceName, options.source);
-			}
+            this.handles = {};
+            this.children = {};
+            if(options.events){
+                this.setEventNames(options.events);
+            }
+            if(options.source){
+                this.setSource(options.sourceName, options.source);
+            }
+            if(options.treeParent){
+               this.treeParent = options.treeParent;
+            }
+
+            var
+                _instanceDispose,
+                _dispose = function(){
+                    this._disposeEvents();
+                    this.DISPOSED = true;
+                    this.dispose = function(){};
+                }.bind(this);
+
+            if(this.dispose){
+                _instanceDispose = this.dispose.bind(this);
+                this.dispose = function(){
+                    _instanceDispose();
+                    _dispose();
+                };
+            }else{
+                this.dispose = _dispose;
+            }
 		},
-	
-		setEventNames: function(events){
-			this.__events = {};
-			for(var key in events){
-				if(events.hasOwnProperty(key)){
-					this.__events[key] = events[key];
-					this.__events[events[key]] = events[key];
-				}
-			}
-		},
-	
-		setSource: function(name, source){
-			this.sourceName = name;
-			this.source = source;
-			if(!name || !source){
-				console.error('A name and a source must be passed');
-			}
-		},
-	
-		removeChild: function(childId){
-			if(this.children[childId]){
-				delete this.children[childId].eventTree;
-				delete this.children[childId];
-			}
-		},
-	
-		child: function(options){
-			options = options || {};
-			options.events = this.__events;
-			var eventTree = new EventTree(options);
-			eventTree.parent = this;
-			this.children[eventTree.id] = {
-				eventTree: eventTree
-			};
-			return eventTree;
-		},
-	
-		missingEventName: function(name){
-			console.warn('Possible incorrect event name:  emit('+name+')');
-		},
-		
-		addHandle: function(handle){
-			if(!this.listeners.own){
-				this.listeners.own = [];
-			}
-			this.listeners.own.push(handle);
-		},
-	
+
+        setEventNames: function(events){
+            this.__events = {};
+            for(var key in events){
+                if(events.hasOwnProperty(key)){
+                    this.__events[key] = events[key];
+                    this.__events[events[key]] = events[key];
+                }
+            }
+        },
+
+        setSource: function(name, source){
+            this.sourceName = name;
+            this.source = source;
+            if(!name || !source){
+                console.error('A name and a source must be passed');
+            }
+        },
+
+        removeChild: function(childId){
+            delete this.children[childId].tree;
+            delete this.children[childId];
+        },
+
+        child: function(options){
+            options = options || {};
+            options.events = this.__events;
+            var tree = new EventTree(options);
+            tree.parent = this;
+            this.children[tree.id] = {
+                tree: tree
+            };
+            return tree;
+        },
+
+        missingEventName: function(name){
+            console.warn('Possible incorrect event name:  emit('+name+')');
+        },
+
 		emit: function(name, event){
-			if(this.__events && !this.__events[name]){
-				this.missingEventName(name);
-			}
+            if(this.__events && !this.__events[name]){
+                this.missingEventName(name);
+                //console.log(this.events);
+            }
 			var
-				stopBubbling,
+                stopBubbling,
 				key,
 				listeners = this.listeners[name],
 				args = Array.prototype.slice.call(arguments);
-	
+
 			args.shift();
-			
-			if(args){
-				if(Array.isArray(args) && args[0] === ''){
-					//noop
-					// this may be brittle...
-				}else{
-					this._previousEvents[name] = args;
-				}
-			}
-			
-			// adds ancestors to create a hierarchy
-			// event may look like:
-			// event = {
-			//      section:{...},
-			//      serie:{...},
-			//      group:{...},
-			//      graph:{...},
-			//      possibleEventData:{...}
-			//}
-	
-			if(this.source && typeof event === 'object'){
-				event[this.sourceName] = this.source;
-			}
-			
-			// If listener returns false, bubbling stops
+
+            if(this.source && typeof event === 'object'){
+                event[this.sourceName] = this.source;
+            }
+
+            // If listener returns false, bubbling stops
 			if(listeners){
 				for(key in listeners){
 					if(listeners.hasOwnProperty(key)){
-						stopBubbling = listeners[key].apply(null, args);
-						if(stopBubbling === false){
-							break;
-						}
+                        stopBubbling = listeners[key].apply(null, args);
+                        if(stopBubbling === false){
+                            break;
+                        }
 					}
 				}
 			}
-	
-			if(this.parent){
-				this.parent.emit.apply(this.parent, arguments);
-			}
-	
+
+            if(this.parent){
+                this.parent.emit.apply(this.parent, arguments);
+            }
+
+            if(this.treeParent){
+                this.treeParent.emit.apply(this.treeParent, arguments);
+            }
+
 		},
-	
+
 		removeAll: function(){
-			var
-				listeners = this.listeners,
-				handles = this.handles;
-			Object.keys(handles).forEach(function(key){
-				handles[key].remove();
-			});
-			Object.keys(listeners).forEach(function(key){
-				delete listeners[key];
-			});
+            var
+                listeners = this.listeners,
+                handles = this.handles;
+            Object.keys(handles).forEach(function(key){
+                handles[key].remove();
+            });
+            Object.keys(listeners).forEach(function(key){
+                delete listeners[key];
+            });
 			this.listeners = {};
 		},
-	
-		once: function(name, callback, context, includeMissed){
-			var handle = this.on(name, function(){
-				callback.apply(context, arguments);
-				handle.remove();
-			}, this);	
-		},
-		
-		on: function(name, callback, context, includeMissed){
-			var
-				handles = this.handles,
-				handle,
+
+		on: function(name, callback, context, handleId){
+            if(this.__events && !this.__events[name]){
+                console.warn('Possible incorrect event name:  on('+name+')');
+            }
+            var
+                handles = this.handles,
+                handle,
 				listeners = this.listeners,
 				paused,
-				deferredResult,
 				id = uid('listener-');
-			if(dom.isNode(name) || dom.isWindow(name)){
-			//if(typeof name !== 'string' && (name instanceof window.Node || name === window)){
-				handle = on.apply(null, arguments);
-				// need to collect and destroy
-				return handle;
-			}
-			if(this.__events && !this.__events[name]){
-				console.warn('Possible incorrect event name:  on('+name+')');
-			}
-			
-			if(context === true){
-				includeMissed = true;
-				context = false;
-			}
-		
+
 			this.listeners[name] = this.listeners[name] || {};
 			if(context){
-				callback = callback.bind(context);
+                if(typeof context === 'string'){
+                    handleId = context;
+                }else{
+                    callback = callback.bind(context);
+                }
 			}
-	
+
 			listeners[name][id] = callback;
 
-			if(includeMissed && this._previousEvents[name]){
-				deferredResult = this._previousEvents[name];
-				//setTimeout(function(){
-					callback(deferredResult);
-				//}.bind(this), 100);
-				this._previousEvents[name] = null; // so it won't fire again... I think...
-			}
-	
 			handle = {
-				id: uid('handle'),
+                id: uid('handle'),
 				remove: function(){
-					this.pause();
-					delete listeners[name][id];
-					delete handles[this.id];
-					paused = noop;
+                    this.pause();
+                    if(listeners[name]){
+                        delete listeners[name][id];
+                        delete handles[this.id];
+                    }
+                    paused = noop;
 				},
 				pause: function(){
-					paused = listeners[name][id];
-					listeners[name][id] = noop;
+                    if(listeners[name]){
+                        // if disposed before calling this, listeners no longer
+                        // exist
+                        paused = listeners[name][id];
+                        listeners[name][id] = noop;
+                    }
 				},
 				resume: function(){
-					listeners[name][id] = paused;
-				}
+                    if(paused){
+                        listeners[name][id] = paused;
+                    }
+                }
 			};
-	
-			handles[handle.id] = handle;
-			return handle;
+
+            handles[handle.id] = handle;
+
+            if(handleId){
+                register(handleId, handle);
+            }
+
+            return handle;
 		},
-	
-		_disposeEvents: function(){
-			// convenience method that can be called instead of
-			// using inheritance on dispose()
-			this.removeAll();
-			if(this.parent){
-				this.parent.removeChild(this.id);
-			}
-			this.events = null;
-		},
-	
-		dispose: function(){
-			this._disposeEvents();
-		}
+
+        once: function(name, callback, context, handleId){
+            if(context && typeof context === 'object'){
+                callback = callback.bind(context);
+            }
+            if(handleId){
+                console.warn('handleId not supported for `once`');
+            }
+            var handle = this.on(name, function(){
+                callback.apply(null, arguments);
+                handle.remove();
+            });
+
+            return handle;
+        },
+
+        _disposeEvents: function(){
+            // convenience method that can be called instead of
+            // using inheritance on dispose()
+            this.removeAll();
+            if(this.parent){
+                this.parent.removeChild(this.id);
+            }
+            this.events = null;
+        }
 	});
-	
-	return EventTree;
-	});
+
+    EventTree.removeById = function(handleId){
+        if(registry[handleId]){
+            //console.log('remove handleId', handleId);
+            registry[handleId].forEach(function(h){ h.remove(); });
+            registry[handleId] = null;
+            delete registry[handleId];
+        }else{
+            console.error('handleId not found in registry', handleId, registry);
+        }
+    };
+    return EventTree;
+});
